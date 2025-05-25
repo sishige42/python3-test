28d727f9bf638acdaef6cdfe08d09cc822e18d02
sishige <peanutfromchiba@icloud.com>
2025-05-25 23:57:16 +0900

    コマンドライン引数で複数ファイルの受け取り

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ import argparse
 
 def openfile():
     parser = argparse.ArgumentParser(description="CSVファイル名を受け取るサンプル")
-    parser.add_argument("filename", help="読み込むCSVファイル名")
+    parser.add_argument("files", nargs="+", help="読み込むCSVファイル名")
     args = parser.parse_args()
     return args
 
 args = openfile()
-print(f"指定されたファイル名: {args.filename}")
+for filename in args.files:
+    print(f"指定されたファイル名: {filename}")
